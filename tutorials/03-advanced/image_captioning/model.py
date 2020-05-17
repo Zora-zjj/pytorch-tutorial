71c8c342d97190959bb70de189c7ddabcb7e70d6
Zora-zjj <54018191+Zora-zjj@users.noreply.github.com>
2020-05-17 23:10:27 +0800

    Update model.py

--- tutorials/03-advanced/image_captioning/model.py
+++ tutorials/03-advanced/image_captioning/model.py
@@ -19,11 +19,11 @@ class EncoderCNN(nn.Module):
         with torch.no_grad():
             features = self.resnet(images)
         features = features.reshape(features.size(0), -1)     #维度  [batch,打平]
-        features = self.bn(self.linear(features))             #维度  [batch,embed_size]
+        features = self.bn(self.linear(features))             #维度  [batch,embed_size]，即lstm的x0
         return features
 
 
-class DecoderRNN(nn.Module):  #input数据(seq_len,batch_size,input_size)=(,,embed_size)：(每个句子长度，几个句子，每个单词用几维度表示)
+class DecoderRNN(nn.Module):  #input数据(seq_len,batch_size,input_size)=(,,embed_size)：(每个句子长度，几个句子，每个单词用几维度表示),batch放最前
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers, max_seq_length=20): 
         """Set the hyper-parameters and build the layers."""
         super(DecoderRNN, self).__init__()
@@ -34,23 +34,23 @@ class DecoderRNN(nn.Module):  #input数据(seq_len,batch_size,input_size)=(,,emb
         
     def forward(self, features, captions, lengths):
         """Decode image feature vectors and generates captions."""
-        embeddings = self.embed(captions)
-        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)   # [batch,1,embed_size]+[batch,vocab_size,embed_size] 将x0放在xt之后
-        packed = pack_padded_sequence(embeddings, lengths, batch_first=True)   #pack_padded_sequence填充
+        embeddings = self.embed(captions)   #captions是单词id号列表，[句子数量，单词数量，embed_size]，同上面input数据
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)   # [batch,1,embed_size]+[batch,seq_len,embed_size] 将x0放在xt之前
+        packed = pack_padded_sequence(embeddings, lengths, batch_first=True)   #pack_padded_sequence填充,和data_loader里的补长操作重复？？？lengths作用？？？
         hiddens, _ = self.lstm(packed)     #hiddens是out，维度[batch,seq_len,h dim]
-        outputs = self.linear(hiddens[0])  # hidden维度[1,2,3] , hidden[0]维度[2,3] ???????? out 是什么？？？？
+        outputs = self.linear(hiddens[0])  # hidden维度[1,2,3] , hidden[0]维度[2,3]。 outputs维度[batch,seq_len,vocab_size],变成单词embed长度
         return outputs
     
-    def sample(self, features, states=None):
+    def sample(self, features, states=None):    #greedy search，上面是直接用groundtruth当做生成下一个单词的条件
         """Generate captions for given image features using greedy search."""
         sampled_ids = []
-        inputs = features.unsqueeze(1)                           # [batch,1,embed_size]
+        inputs = features.unsqueeze(1)                           # inputs:[batch,1,embed_size]
         for i in range(self.max_seg_length):
-            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)？？？？？
+            hiddens, states = self.lstm(inputs, states)          # hiddens: (batch_size, 1, hidden_size)
             outputs = self.linear(hiddens.squeeze(1))            # outputs:  (batch_size, vocab_size)
-            _, predicted = outputs.max(1)                        # predicted: (batch_size)
+            _, predicted = outputs.max(1)                        # predicted: (batch_size)，在vocab_size上取最大值，即选择一个概率最大单词
             sampled_ids.append(predicted)
-            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)
+            inputs = self.embed(predicted)                       # inputs: (batch_size, embed_size)，将这个单词当做下个输入
             inputs = inputs.unsqueeze(1)                         # inputs: (batch_size, 1, embed_size)
         sampled_ids = torch.stack(sampled_ids, 1)                # sampled_ids: (batch_size, max_seq_length)
         return sampled_ids
